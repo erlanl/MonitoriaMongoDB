5f6c2010b63e69dd24589c8187f59d9099ca80c0
Amanda-Quirino <aqrs@cin.ufpe.br>
2023-04-23 18:35:07 -0300

    fix: Modificando povoamento de Aulas

--- povoamentoAulas.js
+++ povoamentoAulas.js
@@ -2,6 +2,7 @@ db = db.getSiblingDB("monitoria");
 db.aulas.drop();
 db.aulas.insertMany([
     {
+        codigo: 1,
         disciplina: "IF669",
         monitores: [11122233344, 20220040],
         data: new Date('2022-03-03'),
@@ -10,38 +11,43 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 2,
         disciplina: "IF669",
         monitores: [11122233344],
-        data: new Date('2020-01-10'),
+        data: new Date('2022-01-10'),
         duracao_em_horas: 2,
         conteudo: "Comandos condicionais"
     },
 
     {
+        codigo: 3,
         disciplina: "IF669",
         monitores: [20220040],
-        data: new Date('2022-04-22'),
+        data: new Date('2022-07-22'),
         duracao_em_horas: 2,
         conteudo: "Funções"
     },
     
     {
+        codigo: 4,
         disciplina: "IF668",
         monitores: [20220030],
-        data: new Date('2021-04-21'),
+        data: new Date('2022-04-21'),
         duracao_em_horas: 2,
         conteudo: "HTML e CSS"
     },
 
     {
+        codigo: 5,
         disciplina: "IF668",
         monitores: [20220030],
-        data: new Date('2021-02-13'),
+        data: new Date('2022-08-13'),
         duracao_em_horas: 2,
         conteudo: "Latex"
     },
 
     {    
+        codigo: 6,
         disciplina: "MA531",    
         monitores: [22233344455],
         data: new Date('2022-02-01'),
@@ -50,6 +56,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 7,
         disciplina: "IF685",
         monitores: [93322442122, 92200011122],
         data: new Date('2023-03-02'),
@@ -58,6 +65,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 8,
         disciplina: "IF672",
         monitores: [55566677788, 20220060, 44455566677],
         data: new Date('2022-04-18'),
@@ -66,6 +74,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 9,
         disciplina: "IF674",
         monitores: [16171819202],
         data: new Date('2022-02-20'),
@@ -74,6 +83,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 10,
         disciplina: "IF677",
         monitores: [17181920212],
         data: new Date('2023-03-25'),
@@ -82,6 +92,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 11,
         disciplina: "IF677",
         monitores: [17181920212],
         data: new Date('2023-05-15'),
@@ -90,6 +101,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 12,
         disciplina: "IF670",
         monitores: [33344455566, 20220050],
         data: new Date('2022-05-05'),
@@ -98,6 +110,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 13,
         disciplina: "IF670",
         monitores: [33344455566],
         data: new Date('2022-05-12'),
@@ -106,6 +119,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 14,
         disciplina: "IF670",
         monitores: [20220050],
         data: new Date('2022-05-19'),
@@ -114,6 +128,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 15,
         disciplina: "FI582",
         monitores: [13141516171],
         data: new Date('2022-05-05'),
@@ -122,6 +137,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 16,
         disciplina: "FI582",
         monitores: [13141516171],
         data: new Date('2022-05-19'),
@@ -130,6 +146,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 17,
         disciplina: "IF673",
         monitores: [14151617181],
         data: new Date('2022-05-12'),
@@ -138,6 +155,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 18,
         disciplina: "IF675",
         monitores: [66677788899],
         data: new Date('2022-05-05'),
@@ -146,6 +164,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 19,
         disciplina: "IF675",
         monitores: [66677788899],
         data: new Date('2022-05-19'),
@@ -154,6 +173,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 20,
         disciplina: "IF678",
         monitores: [15161718191],
         data: new Date('2022-05-12'),
@@ -162,6 +182,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 21,
         disciplina: "IF682",
         monitores: [88899900011],
         data: new Date('2022-05-05'),
@@ -170,6 +191,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 22,
         disciplina: "IF682",
         monitores: [88899900011],
         data: new Date('2022-05-12'),
@@ -178,6 +200,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 23,
         disciplina: "IF682",
         monitores: [88899900011],
         data: new Date('2022-05-19'),
@@ -185,7 +208,8 @@ db.aulas.insertMany([
         conteudo: "Engenharia de Requisitos"
     },
 
-    {        
+    {     
+        codigo: 24,   
         disciplina: "IF689",        
         monitores: [10111213141],
         data: new Date('2022-05-03'),
@@ -194,6 +218,7 @@ db.aulas.insertMany([
     },
 
     {
+        codigo: 25,
         disciplina: "IF689",
         monitores: [10111213141],
         data: new Date('2022-03-10'),
