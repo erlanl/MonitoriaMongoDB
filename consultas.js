1227e8dbf01f1951621b5edda5b1630ba0bb4a67
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2023-04-24 20:04:35 -0300

    feat: add consulta utilizando , , , , ,

---
+++ consultas.js
@@ -0,0 +1,36 @@
+//Selecione o nome e código das disciplina das monitorias que possuem 2 ou mais monitores
+//$size, $exists, $eq, $gte, $find, $WHERE
+db.disciplinas.find({
+    codigo: {$in:db.monitoria.distinct("disciplina", {
+        monitores: { $exists: true, $ne: [] },
+        $expr: { $gte: [ { $size: "$monitores" }, 2 ] }
+    })}
+
+}, {nome: true, codigo: true, _id: false})
+
+//USE
+//AGGREGATE
+//MATCH
+//PROJECT
+//GROUP
+//SUM
+//COUNT
+//MAX
+//AVG
+//SORT
+//LIMIT
+//MAPREDUCE
+//FUNCTION
+//PRETTY
+//ALL
+//SET
+//TEXT
+//SEARCH
+//FILTER
+//UPDATE
+//SAVE
+//RENAMECOLLECTION
+//COND
+//LOOKUP
+//FINDONE
+//ADDTOSET
