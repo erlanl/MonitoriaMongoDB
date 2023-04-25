ff307ff3fd24c5ab0a0b23f6257c63cb27dc873b
Rodrigo Rossiter <93346177+RRossiteRR@users.noreply.github.com>
2023-04-24 22:04:14 -0300

    Add consulta com Update, Pretty e Set

--- consultas.js
+++ consultas.js
@@ -1,5 +1,5 @@
-//Selecione o nome e código das disciplina das monitorias que possuem 2 ou mais monitores
-//$size, $exists, $eq, $gte, $find
+// Selecione o nome e código das disciplina das monitorias que possuem 2 ou mais monitores
+// $size, $exists, $eq, $gte, $find
 db.disciplinas.find({
     codigo: {$in:db.monitoria.distinct("disciplina", {
         monitores: { $exists: true, $ne: [] },
@@ -8,7 +8,6 @@ db.disciplinas.find({
 
 }, {nome: true, codigo: true, _id: false})
 
-//USE
 // Calcula a média da duração das monitorias de cada disciplina de 2022.1
 // aggregate, match, group, avg, sort
 db.aulas.aggregate([
@@ -17,6 +16,12 @@ db.aulas.aggregate([
     { $sort: {avgValue: -1}}
 ])
 
+// Atualiza no banco de dados o professor que leciona a disciplina de introdução à computação
+// Update, Pretty, Set
+db.disciplinas.updateOne({codigo: "IF668"}, {$set: {professor: "Filipe Calegario"}})
+db.disciplinas.find({codigo: "IF668"}).pretty() // exibe os dados da disciplina de introdução à computação, mostrando que a alteração foi feita
+
+//USE
 //PROJECT
 //SUM
 //COUNT
@@ -24,14 +29,11 @@ db.aulas.aggregate([
 //LIMIT
 //MAPREDUCE
 //FUNCTION
-//PRETTY
 //ALL
-//SET
 //TEXT
 //$WHERE
 //SEARCH
 //FILTER
-//UPDATE
 //SAVE
 //RENAMECOLLECTION
 //COND
