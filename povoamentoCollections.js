4be0a7c45b246f87fbacaef0e4c8168d1ac7869b
erlanl <erlanlirajr@gmail.com>
2023-04-24 21:08:08 -0300

    fix: arrumando declaração

--- povoamentoCollections.js
+++ povoamentoCollections.js
@@ -1,4 +1,5 @@
 db = db.getSiblingDB("monitoria");
+
 //Dropando todas as collections
 db.aulas.drop();
 db.disciplinas.drop();
@@ -552,96 +553,96 @@ db.disciplinas.insertMany([
 //Povoamento FeedBack
 db.feedback.insertMany([
     {
-      matricula: 11122233344,
+      matricula_monitor: 11122233344,
       disciplina: "IF669",
       periodo: "2022.1",
       feedback: "Foi um semestre intenso, mas aprendi muito com os desafios da monitoria. Me esforcei para auxiliar os alunos da melhor maneira possível e fico feliz com os resultados alcançados."
     },
     
     {
-      matricula: 22233344455,
+      matricula_monitor: 22233344455,
       disciplina: "MA531",
       periodo: "2022.1",
       feedback: "Tive um semestre bastante produtivo como monitor, consegui ajudar muitos alunos a entenderem a disciplina e a terem um melhor desempenho. Aprendi muito com os professores e com os colegas de trabalho."
     },
 
     {
-      matricula: 33344455566,
+      matricula_monitor: 33344455566,
       disciplina: "IF670",
       periodo: "2022.1",
       feedback: "Foi uma experiência muito enriquecedora ser monitora nesse semestre. Tive a oportunidade de colocar em prática meus conhecimentos e ajudar os alunos a entenderem os conteúdos. Com certeza contribuí para a formação de muitos futuros profissionais da área."
     },
 
     {
-      matricula: 44455566677,
+      matricula_monitor: 44455566677,
       disciplina: "IF672",
       periodo: "2022.1",
       feedback: "Me dediquei muito durante esse semestre como monitora, tanto nas aulas quanto nas atividades extras. Foi gratificante ver o progresso dos alunos e saber que pude ajudá-los a terem um melhor desempenho na disciplina."
     },
 
     {
-      matricula: 55566677788,
+      matricula_monitor: 55566677788,
       disciplina: "IF672",
       periodo: "2022.1",
       feedback: "Foi uma honra ser monitor nesse semestre. Aprendi muito com os alunos e com os outros monitores, além de ter tido a oportunidade de me aprofundar nos conteúdos da disciplina. Espero ter contribuído para o sucesso dos alunos na disciplina."
     },
 
     {
-      matricula: 66677788899,
+      matricula_monitor: 66677788899,
       disciplina: "IF675",
       periodo: "2022.1",
       feedback: "Foi um semestre desafiador como monitora, mas pude aprender muito e evoluir bastante ao longo do período. Agradeço aos colegas de trabalho e aos professores por todo o apoio e suporte oferecidos."
     },
     {
-      matricula: 77788899900,
+      matricula_monitor: 77788899900,
       disciplina: "IF679",
       periodo: "2022.1",
       feedback: "Ser monitor foi uma experiência incrível nesse semestre. Tive a oportunidade de ajudar os alunos e de me aprofundar ainda mais nos conteúdos da disciplina. Agradeço aos colegas de trabalho e aos professores pela parceria e pelo aprendizado mútuo."
     },
 
     {
-      matricula: 88899900011,
+      matricula_monitor: 88899900011,
       disciplina: "IF682",
       periodo: "2022.1",
       feedback: "Foi um semestre intenso como monitora, mas consegui cumprir todas as minhas obrigações e ajudar os alunos da melhor maneira possível. Agradeço aos professores e aos colegas de trabalho pela parceria."
     },
 
     {
-        matricula: 99900011122,
+        matricula_monitor: 99900011122,
         disciplina: "IF685",
         periodo: "2022.1",
         feedback: "Foi um semestre muito produtivo! Tive a oportunidade de trabalhar com alunos muito dedicados e ver como a monitoria pode fazer a diferença em suas vidas acadêmicas."
     },
     {
-        matricula: 92200011122,
+        matricula_monitor: 92200011122,
         disciplina: "IF685",
         periodo: "2022.1",
         feedback: "Tive um período bastante intenso e produtivo como monitora. Consegui aplicar meu conhecimento na disciplina e auxiliar os alunos a superarem suas dificuldades. Fico grata pela oportunidade de ter feito parte desse processo de aprendizagem."
     },
 
     {
-        matricula: 93322442122,
+        matricula_monitor: 93322442122,
         disciplina: "IF685",
         periodo: "2022.1",
         feedback: "Foi um período de muitos desafios, mas também de muito aprendizado e crescimento como monitora. Agradeço aos alunos pela confiança e por terem me permitido fazer parte desse processo de aprendizagem conjunto."
     },
 
     {
-        matricula: 10111213141,
+        matricula_monitor: 10111213141,
         disciplina: "IF689",
         periodo: "2022.1",
         feedback: "Foi um período muito produtivo como monitora. Consegui ajudar os alunos a superarem suas dificuldades na disciplina e aprendi muito com eles também. Fico feliz em ter contribuído para o seu sucesso acadêmico."
     },
 
     {
-        matricula: 11111222233,
+        matricula_monitor: 11111222233,
         disciplina: "IF681",
         periodo: "2022.1",
         feedback: "Foi um período de muito aprendizado e crescimento como monitor. Consegui aplicar meu conhecimento na disciplina e auxiliar os alunos a superarem suas dificuldades. Fico feliz em ter contribuído para o sucesso acadêmico dos alunos."
     },
 
     {
-        matricula: 12121212121,
+        matricula_monitor: 12121212121,
         disciplina: "IF680",
         periodo: "2022.1",
         feedback: "Foi um período desafiador como monitora, mas também muito gratificante. Aprendi bastante sobre a disciplina e também sobre a arte de ensinar. Fico feliz em ter ajudado os alunos a se desenvolverem e a alcançarem seus objetivos."
