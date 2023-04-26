9b32cddcb4f4f1cf89f69efbf8e0d5778887bae3
Erlan Lira <94120629+erlanl@users.noreply.github.com>
2023-04-25 23:35:45 -0300

    fix: alterações finais

--- consultas.js
+++ consultas.js
@@ -20,27 +20,43 @@ db.aulas.aggregate([
 
 // Atualiza no banco de dados o professor que leciona a disciplina de introdução à computação
 // Update, Pretty, Set
+db.disciplinas.find({codigo: "IF668"}).pretty() // exibe os dados antes da alteração
 db.disciplinas.updateOne({codigo: "IF668"}, {$set: {professor: "Filipe Calegario"}})
 db.disciplinas.find({codigo: "IF668"}).pretty() // exibe os dados da disciplina de introdução à computação, mostrando que a alteração foi feita
 
 // Atualiza no banco de dados o professor de Informática Teórica
+db.disciplinas.find({codigo: "IF689"}).pretty()
 db.disciplinas.updateOne({codigo: "IF689"}, {$set: {professor: "Ruy de Queiroz"}})
 db.disciplinas.find({codigo: "IF689"}).pretty()
 
 //Retorne o top 3 dos monitores com maiores feedbacks, retornem também a quantidade de caracteres do feedback, nome, email e matricula do monitor, assim como o código da discplina monitorada por ele.
 //LIMIT, LOOKUP, PROJECT
 db.feedback.aggregate([
-    {$project: {_id: 0, matricula_monitor: 1, disciplina: 1, email: 1, feedback_length: {$strLenCP: "$feedback"}}},
-    {$sort: {feedback_length: -1}},
-    {$lookup: {
-      from: "monitores",
-      localField: "matricula_monitor",
-      foreignField: "matricula",
-      as: "monitor_info"
-    }},
-    {$unwind: "$monitor_info"},
-    {$project: {matricula_monitor: "$monitor_info.matricula",  nome_monitor: "$monitor_info.nome", email_monitor: "$monitor_info.email", disciplina_monitorada: "$monitor_info.disciplina", feedback_length: 1}},
-    {$limit: 3}
+    
+  // O método project() é usado para selecionar campos de coleção de feedback
+  // O operador $strLenCP é usado para obter o tamanho do campo "feedback" em caracteres
+  {$project: {_id: 0, matricula_monitor: 1, disciplina: 1, email: 1, feedback_length: {$strLenCP: "$feedback"}}},
+  
+  // Ordena de acordo com o tamanho do feedback em ordem decrescente
+  {$sort: {feedback_length: -1}},
+  
+  // O método lookup() é usado para combinar documentos da coleção de monitores com de feedback
+  // O campo "matricula_monitor" em feedback é comparado com o campo "matricula" em monitores
+  {$lookup: { 
+    from: "monitores",
+    localField: "matricula_monitor",
+    foreignField: "matricula",
+    as: "monitor_info"
+  }},
+  
+  // O método unwind() é usado para desestruturar o array "monitor_info" em vários documentos
+  {$unwind: "$monitor_info"},
+  
+  // O método project() é usado novamente para selecionar campos específicos da coleção "monitor_info"
+  {$project: {matricula: "$monitor_info.matricula",  monitor: "$monitor_info.nome", email: "$monitor_info.email", disciplina: "$monitor_info.disciplina", feedback_length: 1}},
+  
+  // O método limit() é usado para limitar para retornar apenas 3 documentos (ou seja, o top 3)
+  {$limit: 3}
 ])
 
 //Retorne a matricula e o feedback dos monitores que informaram que tiveram um "semestre intenso"
@@ -48,9 +64,11 @@ db.feedback.aggregate([
 db.feedback.createIndex({ feedback: "text" })
 db.feedback.find({ $text: { $search: "\"semestre intenso\"" } }, {matricula_monitor: true, feedback: true, _id: false})
 
-// Adicionando o monitor de matricula 20220040 na aula de codigo 2
+// Adicionando um monitor na aula de codigo 2
 // addToSet
+db.aulas.find({codigo: 2}).pretty()
 db.aulas.updateOne({codigo: 2}, {$addToSet: {monitores: 20220040}})
+db.aulas.find({codigo: 2}).pretty()
 
 // Retorna a quantidade de aulas que tiveram mais de 3 horas de duracao
 // $where, function, count
@@ -78,7 +96,7 @@ db.monitoria.aggregate(
     }}
 )
 
-//retorna o nome e a quantidade de horas de monitoria que cada monitor ministrou
+//Retorna o nome e a quantidade de horas de monitoria que cada monitor ministrou
 //SUM
 db.aulas.aggregate([
     {$lookup: {
@@ -123,7 +141,7 @@ db.disciplinas.aggregate([
     }
 ])
 
-// Retornando o nome e o número de disciplina de cada professor 
+// Retornando o nome e o número de disciplina de cada professor 	
 db.disciplinas.mapReduce(
   function() {
     emit(this.professor, 1);
